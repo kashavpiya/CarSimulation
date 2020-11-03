e588fc0c15aa4f465c86b37bfc7e1c2fe9516e40
kashavpiya <71453613+kashavpiya@users.noreply.github.com>
2020-11-03 10:45:28 -0600

    Update Cars.java
    End Msg

--- Cars.java
+++ Cars.java
@@ -1,9 +1,12 @@
 import java.io.FileInputStream;
+import java.util.concurrent.TimeUnit;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.*;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
@@ -26,6 +29,10 @@ public class Cars extends Application {
     
     private static final String BACK_IMAGE2 = "https://images-ext-1.discordapp.net/external/TPR7IrckC7Tc9gPIgofWzXsYJpm-VlRzChpaMJMffYU/https/www.ajaybadgujar.com/wp-content/uploads/2014/06/road.png?widt";
     
+    private static final String GAME_OVER = "https://www.flaticon.com/premium-icon/icons/svg/2066/2066796.svg";
+    
+  //  private static final String GAME_OVER = "http://pngimg.com/uploads/game_over/game_over_PNG51.png";
+    private boolean crashed = false;
     private Image carImage;
     private Node  gCar;
     
@@ -35,12 +42,18 @@ public class Cars extends Application {
     private Image backImage2;
     private Node back2;
     
-
+    private Image gameOver;
+    private Node over;
+    
+    private int count = 0;
+    
     boolean running, goNorth, goSouth, goEast, goWest;
 
     @Override
     public void start(Stage stage) throws Exception {
-        carImage = new Image(CAR_IMAGE);
+    	
+    	
+    	carImage = new Image(CAR_IMAGE);
         gCar = new ImageView(carImage);
         gCar.setRotate(270);
         
@@ -50,13 +63,16 @@ public class Cars extends Application {
         backImage2 = new Image(BACK_IMAGE);
         back2 = new ImageView(backImage);
       
-  
+        gameOver = new Image(GAME_OVER);
+        over = new ImageView(gameOver);
        
-        Group root = new Group(back, back2, gCar);
+        Group root = new Group(back, back2, gCar, over);
         
-        moveGCarTo(3 * W / 4, 3 * H / 4);
+        moveGCarTo(W/2, 3 * H / 4);
         moveBgTo(0 , H/2);
         moveBg2To(0, -H/2);
+        moveOverTo(W, H);
+     
   
         
         Scene scene = new Scene(root, W, H, Color.FORESTGREEN);
@@ -98,10 +114,10 @@ public class Cars extends Application {
             public void handle(long now) {
                 int dx = 0, dy = 0;
 
-                if (goNorth) dy -= 1;
-                if (goSouth) dy += 1;
-                if (goEast)  dx += 1;
-                if (goWest)  dx -= 1;
+                if (goNorth) dy -= 3;
+                if (goSouth) dy += 3;
+                if (goEast)  dx += 3;
+                if (goWest)  dx -= 3;
                 if (running) { dx *= 3; dy *= 3; }
 
                 moveGCarBy(dx, dy);
@@ -110,8 +126,12 @@ public class Cars extends Application {
           //      moveBCarBy(0, 1);
             }
         };
-        timer.start();
+        
+        	timer.start();
+        
+       
     }
+   
 
     private void moveGCarBy(int dx, int dy) {
         if (dx == 0 && dy == 0) return;
@@ -155,13 +175,25 @@ public class Cars extends Application {
     private void moveGCarTo(double x, double y) {
         final double cx = gCar.getBoundsInLocal().getWidth()  / 2;
         final double cy = gCar.getBoundsInLocal().getHeight() / 2;
-
+        
+        boolean accident = isOutOfBounds(x);
+        if (accident == true) {
+        	try {
+				theEnd();
+				
+		//		System.exit(0);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        }
+        
         if (x - cx >= 0 &&
             x + cx <= W &&
             y - cy >= 0 &&
             y + cy <= H) {
         	gCar.relocate(x - cx, y - cy);
-        }
+        } 
     }
     
     private void moveBgTo(double x, double y) {
@@ -191,6 +223,39 @@ public class Cars extends Application {
          	
          }
      }
+    
+    private void moveOverTo(double x, double y) {
+        over.relocate(x,y);
+      }
+    
+    private boolean isOutOfBounds(double x) {
+    	if (x > 384 || x < 128) {
+    		crashed = true;
+    		return true;
+    	}else {
+    		return false;
+    	}
+    }
+    
+    private void theEnd() throws InterruptedException{
+    	crashed = true;
+    	
+    	Alert alert = new Alert(AlertType.WARNING);
+    	alert.setTitle("GAME OVER!");
+    	alert.setHeaderText("You lost the game! Try again.");
+    	alert.setContentText("You just went off the road!");
+    	
+    	alert.show();
+    	
+
+    	count += 1;
+    	
+    	if (count == 100) {
+    		System.exit(0);
+    	}
+    	
+    }
+  
   
     public static void main(String[] args) { launch(args); }
 } 
